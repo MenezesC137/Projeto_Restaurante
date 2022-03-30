d5ca85afd7cdaffd3d76d5dfa2fcaf251e6d5248
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-03-30 11:42:00 -0300

    MY15

--- saboroso/routes/admin.js
+++ saboroso/routes/admin.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var users = require('./../inc/users')
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
@@ -7,9 +8,30 @@ router.get('/', function(req, res, next) {
 
 });
 
+router.post('/login', function(req, res, next) {
+  
+    if (!req.body.email) {
+        users.render(req, res, "Preencha o campo E-mail")
+    }else if (!req.body.password) {
+        users.render(req, res, "Preencha o campo Senha")
+    } else {
+
+        users.login(req.body.email, req.body.password).then(user =>{
+
+            req.session.user = user;
+
+            res.redirect('/admin')
+
+        }).catch (err =>{
+            users.render(req, res, err.message || err)
+        })
+    }
+
+});
+
 router.get('/login', function(req, res, next) {
   
-    res.render('admin/login');
+    users.render(req, res, null)
 
 });
 
