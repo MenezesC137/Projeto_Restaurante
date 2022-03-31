f90a886d947b0bf812b9110044f4eb3d8c52ef08
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-03-31 12:19:09 -0300

    MY32

--- saboroso/routes/admin.js
+++ saboroso/routes/admin.js
@@ -3,7 +3,10 @@ var users = require('./../inc/users')
 var admin = require('./../inc/admin')
 var menus = require('./../inc/menus')
 var reservations = require('./../inc/reservations')
+var moment = require('moment');
 var router = express.Router();
+
+moment.locale('pt-BR')
 
 router.use(function(req, res, next){
 
@@ -132,7 +135,8 @@ router.get('/reservations', function(req, res, next) {
 
         res.render('admin/reservations', admin.getParamns(req, {
             date: {},
-            data
+            data,
+            moment
         }))
 
     })
