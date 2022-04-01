09bd6c22f2e1ed5caae2feae369225585d0908de
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-04-01 11:33:42 -0300

    MY41

--- saboroso/inc/users.js
+++ saboroso/inc/users.js
@@ -46,79 +46,109 @@ module.exports = {
 
             conn.query("SELECT * FROM tb_users ORDER BY name", (err, results) => {
 
-                if(err) {
-            
-                  reject(err);
-            
+                if (err) {
+
+                    reject(err);
+
                 }
 
                 resolve(results);
-            
-              });
+
+            });
         });
     },
-  
+
     save(fields, files) {
-      
+
         return new Promise((resolve, reject) => {
-  
-          let query, params = [
-              fields.name,
-              fields.email
+
+            let query, params = [
+                fields.name,
+                fields.email
             ];
-  
-          if(parseInt(fields.id) > 0) {
-  
-            params.push(fields.id);
-            
-            query = `
+
+            if (parseInt(fields.id) > 0) {
+
+                params.push(fields.id);
+
+                query = `
               UPDATE tb_users
               SET 
                   name = ?,
                   email = ?
               WHERE id = ?
             `;
-  
-          } else {
-  
-            query = `
+
+            } else {
+
+                query = `
               INSERT INTO tb_users (name, email, password)
               VALUES(?, ?, ?)
             `;
-  
-            params.push(fields.password);
-  
-          }
-          
-          conn.query(query, params, (err, results) => {
-            
-            if(err) {
-              reject(err);
-            } else {
-              resolve(results);
+
+                params.push(fields.password);
+
             }
-  
-          });
-        });
-      },
-  
-    delete(id){
-  
-        return new Promise((resolve, reject) =>{
-  
-            conn.query(`
-                DELETE FROM tb_users WHERE id = ?
-            `, [
-                id
-            ], (err, results)=>{
-  
-                if(err) {
+
+            conn.query(query, params, (err, results) => {
+
+                if (err) {
                     reject(err);
                 } else {
                     resolve(results);
                 }
-  
+
+            });
+        });
+    },
+
+    delete(id) {
+
+        return new Promise((resolve, reject) => {
+
+            conn.query(`
+                DELETE FROM tb_users WHERE id = ?
+            `, [
+                id
+            ], (err, results) => {
+
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(results);
+                }
+
             })
+        })
+    },
+
+    changePassword(req) {
+
+        return new Promise((resolve, reject) => {
+
+            if (!req.fields.password) {
+                reject("Preencha a senha!")
+            } else if (req.fields.password !== req.fields.passwordConfirm) {
+                reject("Senhas devem ser identicas")
+            } else {
+
+                conn.query(`
+                    UPDATE tb_users
+                    SET password = ?
+                    WHERE id = ?
+                `, [
+                    req.fields.password,
+                    req.fields.id
+                ], (err, results)=>{
+
+                    if(err) {
+                        reject(err.message)
+                    }else {
+                        resolve(results)
+                    }
+
+                })
+            }
         })
     }
 }
