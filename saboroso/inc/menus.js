f72fa153740c0de9272c3f010b741fe8a1177fde
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-04-01 10:09:04 -0300

    MY39

--- saboroso/inc/menus.js
+++ saboroso/inc/menus.js
@@ -3,101 +3,101 @@ let path = require('path');
 
 module.exports = {
 
-    getMenus() {
+  getMenus() {
 
-        return new Promise((resolve, reject) => {
-
-            conn.query("SELECT * FROM tb_menus ORDER BY title", (err, results) => {
-
-                if(err) {
-            
-                  reject(err);
-            
-                }
-
-                resolve(results);
-            
-              });
-        });
-    },
-
-    save(fields, files) {
-      
       return new Promise((resolve, reject) => {
+
+          conn.query("SELECT * FROM tb_menus ORDER BY title", (err, results) => {
+
+              if(err) {
+          
+                reject(err);
+          
+              }
+
+              resolve(results);
+          
+            });
+      });
+  },
+
+  save(fields, files) {
+    
+    return new Promise((resolve, reject) => {
+      
+      fields.photo = `images/${path.parse(files.photo.path).base}`;
+
+      let query, queryPhoto = '', params = [
+          fields.title,
+          fields.description,
+          fields.price
+        ];
+
+      if(files.photo.name) {
+
+        queryPhoto = ',photo = ?';
+        params.push(fields.photo);
+
+      }
+
+      if(parseInt(fields.id) > 0) {
+
+        params.push(fields.id);
         
-        fields.photo = `images/${path.parse(files.photo.path).base}`;
+        query = `
+          UPDATE tb_menus
+          SET 
+              title = ?,
+              description = ?,
+              price = ?
+              ${queryPhoto}
+          WHERE id = ?
+        `;
 
-        let query, queryPhoto = '', params = [
-            fields.title,
-            fields.description,
-            fields.price
-          ];
+      } else {
 
-        if(files.photo.name) {
-
-          queryPhoto = ',photo = ?';
-          params.push(fields.photo);
-
+        if(!files.photo.name) {
+          reject("Envie a foto do prato");
         }
 
-        if(parseInt(fields.id) > 0) {
+        query = `
+          INSERT INTO tb_menus (title, description, price, photo)
+          VALUES(?, ?, ?, ?)
+        `;
 
-          params.push(fields.id);
-          
-          query = `
-            UPDATE tb_menus
-            SET 
-                title = ?,
-                description = ?,
-                price = ?
-                ${queryPhoto}
-            WHERE id = ?
-          `;
-
+      }
+      
+      conn.query(query, params, (err, results) => {
+        
+        if(err) {
+          reject(err);
         } else {
-
-          if(!files.photo.name) {
-            reject("Envie a foto do prato");
-          }
-
-          query = `
-            INSERT INTO tb_menus (title, description, price, photo)
-            VALUES(?, ?, ?, ?)
-          `;
-
+          resolve(results);
         }
-        
-        conn.query(query, params, (err, results) => {
-          
-          if(err) {
-            reject(err);
-          } else {
-            resolve(results);
-          }
-
-        });
 
       });
 
-    },
+    });
 
-    delete(id){
+  },
 
-        return new Promise((resolve, reject) =>{
+  delete(id){
 
-            conn.query(`
-                DELETE FROM tb_menus WHERE id = ?
-            `, [
-                id
-            ], (err, results)=>{
+      return new Promise((resolve, reject) =>{
 
-                if(err) {
-                    reject(err);
-                } else {
-                    resolve(results);
-                }
+          conn.query(`
+              DELETE FROM tb_menus WHERE id = ?
+          `, [
+              id
+          ], (err, results)=>{
 
-            })
-        })
-    }
+              if(err) {
+                  reject(err);
+              } else {
+                  resolve(results);
+              }
+
+          })
+      })
+  }
 }
