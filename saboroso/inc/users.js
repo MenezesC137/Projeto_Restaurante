ada54ff4aed9f7aa27397ce138242e4abcf445a5
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-04-01 10:32:40 -0300

    MY40

--- saboroso/inc/users.js
+++ saboroso/inc/users.js
@@ -43,15 +43,15 @@ module.exports = {
     getUsers() {
 
         return new Promise((resolve, reject) => {
-  
-            conn.query("SELECT * FROM tb_users ORDER BY title", (err, results) => {
-  
+
+            conn.query("SELECT * FROM tb_users ORDER BY name", (err, results) => {
+
                 if(err) {
             
                   reject(err);
             
                 }
-  
+
                 resolve(results);
             
               });
@@ -60,49 +60,47 @@ module.exports = {
   
     save(fields, files) {
       
-      return new Promise((resolve, reject) => {
-      
-        let query, queryPhoto = '', params = [
-            fields.name,
-            fields.email
-        ];
+        return new Promise((resolve, reject) => {
   
-        if(parseInt(fields.id) > 0) {
+          let query, params = [
+              fields.name,
+              fields.email
+            ];
   
-          params.push(fields.id);
-          
-          query = `
-            UPDATE tb_users
-            SET 
-                name = ?,
-                email = ?
-            WHERE id = ?
-          `;
+          if(parseInt(fields.id) > 0) {
   
-        } else {
+            params.push(fields.id);
+            
+            query = `
+              UPDATE tb_users
+              SET 
+                  name = ?,
+                  email = ?
+              WHERE id = ?
+            `;
+  
+          } else {
   
             query = `
-                INSERT INTO tb_user (name, email, password)
-                VALUES(?, ?, ?)
+              INSERT INTO tb_users (name, email, password)
+              VALUES(?, ?, ?)
             `;
-
-            params.push(fields.password)
   
-        }
-        
-        conn.query(query, params, (err, results) => {
-          
-          if(err) {
-            reject(err);
-          } else {
-            resolve(results);
+            params.push(fields.password);
+  
           }
+          
+          conn.query(query, params, (err, results) => {
+            
+            if(err) {
+              reject(err);
+            } else {
+              resolve(results);
+            }
   
+          });
         });
-  
-      });
-  
-    },
+      },
   
     delete(id){
   
