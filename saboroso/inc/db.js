aa563b4f0908d3d6e7013abf68817913597f4fee
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-04-01 12:58:01 -0300

    MY44

--- saboroso/inc/db.js
+++ saboroso/inc/db.js
@@ -4,7 +4,8 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'user',
   database: 'saboroso',
-  password: 'clear'
+  password: 'clear',
+  multipleStatements: true
 });
 
 module.exports = connection
--- saboroso/inc/reservations.js
+++ saboroso/inc/reservations.js
@@ -1,4 +1,5 @@
-var conn = require("./db")
+var conn = require("./db");
+var Pagination = require("./pagination");
 
 module.exports = {
 
@@ -70,24 +71,19 @@ module.exports = {
         })
     },
 
-    getReservations() {
+    getReservations(page) {
 
-        return new Promise((resolve, reject) => {
+        if(!page) page = 1;
 
-            conn.query(
-                `SELECT * FROM tb_reservations ORDER BY date DESC
-            `, (err, results) => {
+        let pag = new Pagination (
 
-                if(err) {
-            
-                  reject(err);
-            
-                }
+            `
+                SELECT SQL_CALC_FOUND_ROWS * FROM tb_reservations ORDER BY name LIMIT ?, ?
+            `
+        )
 
-                resolve(results);
-            
-              });
-        });
+        return pag.getPage(page)
+
     },
 
     delete(id){
